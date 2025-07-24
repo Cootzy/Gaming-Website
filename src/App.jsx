f3481e6d2597cc3844f8d0d91a62f83f0acc64ef
Cootzy <umastryxsubterror@gmail.com>
2025-07-24 23:45:35 +0700

    Implement Header & Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div className="text-5xl text-red-700">App works!</div>;
+  return (
+    <div className="">
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 
 export default App;
